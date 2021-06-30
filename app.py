e2546307dcd3cbcd42d7135e7efd5e452385f4df
aaeiln <31373480+aaeiln@users.noreply.github.com>
2021-06-30 09:19:11 -0700

    Update app.py

---
+++ app.py
@@ -0,0 +1,92 @@
+#import the appropriate libraries
+
+import streamlit as st
+import pandas as pandas
+from PIL import Image
+
+#Add a title and an image
+
+st.write("""Find data on my favorite stocks: Facebook, Tesla, and Microsoft""")
+
+image = Image.open("/Users/alanniegrant/code_for_days/stockapp.png")
+st.image(image, use_column_width = True)
+
+#Create a sidebar header
+
+st.sidebar.header('User Input Choices: FB, TSLA, or MSFT')
+
+#Create a function to get the user's input
+
+def get_input():
+    start_date = st.sidebar.text_input("Start Date", "1986-03-13")
+    start_date = st.sidebar.text_input("End Date", "2021-06-29")
+    stock_symbol = st.sidebar.text_input("Stock Symbol", "FB")
+    return start_, end_date, stock_symbol
+
+#Create a function to get the company name
+
+def get_company_name(symbol):
+    if symbol == 'FB':
+        return 'Facebook'
+    elif symbol == 'TSLA'
+        return 'Tesla'
+    elif symbol == 'MSFT'
+        return 'Microsoft'
+else:
+    'None'
+
+#Create a function to get company data from file
+
+get_data(symbol, start, end):
+if symbol.upper() == 'FB':
+    df = pd.read.csv("/Users/alanniegrant/code_for_days/hist_fb.csv")
+elif symbol.upper() == 'TSLA':
+    df = pd.read.csv("/Users/alanniegrant/code_for_days/hist_tsla.csv")
+elif symbol.upper() == 'MSFT':
+    df = pd.read.csv("/Users/alanniegrant/code_for_days/hist_ms.csv")
+else:
+    df = pf.DataFrame(columns = ('Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'))
+
+#Get the date range
+start = pd.to_datetime(start)
+end = pd.to_datetime(end)
+
+
+#set start and end index rows to 0
+start_row = 0
+end_row = 0
+
+#start the date from the top of the data down
+for i in range(0, len(df)):
+    if start <= pd.to_datetime(df['Date'][i]) ):
+        start_row = i
+        break
+
+for j in range(0, len(df):
+    if end >= pd.to_datetime(df['Date'][len(df)-1-j]):
+        end_row = len(df) -1 -j
+        break
+#set index to the date
+df = df.set_index(pd.DateTimeIndex(df['Date'].values))
+            
+return df.iloc[start_row:end_row +1, :]
+               
+#Get user's input
+start, end, symbol = get_input()
+#get the data
+df = get_data(symbol, start, end)
+
+#company name
+company_name = get_company_name(symbol.upper())
+
+#diplay close price
+st.header(company_name+" Close Price\n")
+st.line_chart(df['Close'])
+               
+               
+#diplay volume
+st.header(company_name+" Volume\n")
+st.line_chart(df['Volume'])
+
+st.header('Data Statistics')
+st.write(df.describe())
